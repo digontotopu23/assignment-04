f9b6aa2d28f0a81ce83f272cd9c633ac5e35694b
digontotopu23 <digontotopu@gmail.com>
2026-02-23 16:16:40 +0600

    create card section in array

---
+++ script.js
@@ -0,0 +1,16 @@
+
+const jobs = [
+  {
+    id: 1,
+    company: "Mobile First Corp",
+    position: "React Native Developer",
+    location: "Remote",
+    type: "Full-time",
+    salary: "$130,000 - $175,000",
+    description: "Build cross-platform mobile apps using React Native.",
+    status: "none"
+  }
+   
+];
+
+ 
